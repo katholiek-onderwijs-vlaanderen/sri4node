679ab41a5b5069cdb225a9178326a8978f55f2a4
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2015-07-13 15:13:18 +0200

    finished spliting context.js

--- test/context.js
+++ test/context.js
@@ -10,11 +10,10 @@ context.serve();
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
-var Q = require('q');
 var common = require('../js/common.js');
 var cl = common.cl;
 
-var $u, $m, $s;
+var $u;
 var configCache = null;
 
 exports = module.exports = {
@@ -51,17 +50,6 @@ exports = module.exports = {
     }
 
     $u = roa.utils;
-    $m = roa.mapUtils;
-    $s = roa.schemaUtils;
-
-    function debug(x) {
-      if (logdebug) {
-        cl(x);
-      }
-    }
-
-    // node-postgres defaults to 10 clients in the pool, but heroku.com allows 20.
-    pg.defaults.poolSize = 20;
 
     var config = {
       // For debugging SQL can be logged.
@@ -89,92 +77,9 @@ exports = module.exports = {
       resources: [
         require('./context/persons.js')(roa, logdebug),
         require('./context/messages.js')(roa, logdebug),
-        require('./context/communities.js')(roa,logdebug),
-        {
-          type: '/transactions',
-          'public': false, // eslint-disable-line
-          map: {
-            transactiontimestamp: {
-              onupdate: $m.now
-            },
-            fromperson: {
-              references: '/persons'
-            },
-            toperson: {
-              references: '/persons'
-            },
-            description: {},
-            amount: {}
-          },
-          secure: [
-                    ],
-          schema: {
-            $schema: 'http://json-schema.org/schema#',
-            title: 'A single transaction between 2 people.',
-            type: 'object',
-            properties: {
-              transactiontimestamp: $s.timestamp('Date and time when the transaction was recorded.'),
-              fromperson: $s.permalink('/persons', 'A permalink to the person that sent currency.'),
-              toperson: $s.permalink('/persons', 'A permalink to the person that received currency.'),
-              description: $s.string('A description, entered by the person sending, of the transaction.'),
-              amount: $s.numeric('The amount of currency sent. ' +
-                                 'This unit is expressed as 20 units/hour, ' +
-                                 'irrelevant of the group\'s currency settings.')
-            },
-            required: ['fromperson', 'toperson', 'description', 'amount']
-          },
-          afterinsert: [
-              function (db, element) {
-                var amount = element.amount;
-                var tokey = element.toperson.href;
-                tokey = tokey.split('/')[2];
-
-                var updateto = $u.prepareSQL();
-                updateto.sql('update persons set balance = (balance + ')
-                  .param(amount).sql(') where key = ').param(tokey);
-                return $u.executeSQL(db, updateto);
-              },
-              function (db, element) {
-                var amount = element.amount;
-                var fromkey = element.fromperson.href;
-                fromkey = fromkey.split('/')[2];
-
-                var updatefrom = $u.prepareSQL();
-                updatefrom.sql('update persons set balance = (balance - ')
-                  .param(amount).sql(') where key = ').param(fromkey);
-                return $u.executeSQL(db, updatefrom);
-              }
-          ],
-          // TODO : Check if updates are blocked.
-          afterupdate: [
-              function () {
-                var deferred = Q.defer();
-                deferred.reject('Updates on transactions are not allowed.');
-                return deferred.promise;
-              }
-            ]
-        },
-        {
-          type: '/table',
-          'public': true, // eslint-disable-line
-          map: {
-            select: {},
-            from: {}
-          },
-          secure: [],
-          schema: {
-            $schema: 'http://json-schema.org/schema#',
-            title: 'A table with protected keywords, to check escaping of sri4node',
-            type: 'object',
-            properties: {
-              select: $s.string(''),
-              from: $s.string('')
-            },
-            required: ['select', 'from']
-          },
-          afterinsert: [],
-          afterupdate: []
-                },
+        require('./context/communities.js')(roa, logdebug),
+        require('./context/transactions.js')(roa),
+        require('./context/table.js')(roa),
         require('./context/selfreferential.js'),
         require('./context/jsonb.js'),
         require('./context/alldatatypes.js')
