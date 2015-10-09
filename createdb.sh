7a6b60ab2a79ed3fcee3a8f501929477dbdeb5c2
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2015-10-09 18:07:00 +0000

    made createdb.sh compatible with Linux & Darwin

--- createdb.sh
+++ createdb.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+if [ $OSTYPE == 'linux-gnu' ]
+then
+  export PSQL_CMD="sudo sudo -u postgres psql $PSQL_VERSION_OPT"
+else
+  export PSQL_CMD="psql -U postgres"
+fi
+echo Operating system $OSTYPE, using postgres command : $PSQL_CMD
+
 # clean database
-cat sql/clean.sql | sudo sudo -u postgres psql $PSQL_VERSION_OPT
+cat sql/clean.sql | $PSQL_CMD
 
 # create database
-cat sql/schema.sql sql/testdata.sql | sudo sudo -u postgres psql $PSQL_VERSION_OPT
+cat sql/schema.sql sql/testdata.sql | $PSQL_CMD
 
 # grant privileges
-cat sql/privileges.sql | sudo sudo -u postgres psql $PSQL_VERSION_OPT
+cat sql/privileges.sql | $PSQL_CMD
