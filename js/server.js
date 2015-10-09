71c9320013c49ffcf17b45a49b329c48fe19d008
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2015-10-09 18:19:38 +0000

    fixed npm run dev

--- js/server.js
+++ js/server.js
@@ -2,7 +2,14 @@
 Run the reference API for the test suite stand alone.
 Just here for convenience.
 */
+var c9hostname = process.env.C9_HOSTNAME; // eslint-disable-line
+var cl = console.log // eslint-disable-line
+var port = process.env.PORT || 5000; //eslint-disable-line
 
-var roa = require('./sri4node');
-var context = require('./test/context.js');
-context.serve(roa, 5000, true, false, false);
+if (c9hostname) {
+  cl('https://' + c9hostname);
+}
+
+var roa = require('../sri4node');
+var context = require('../test/context.js');
+context.serve(roa, port, true, false, false);
