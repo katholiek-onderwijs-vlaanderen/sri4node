34f0d158742368463e1089b2e1b303aad7288777
guntherclaes <guntherclaes@lemonade.be>
2015-10-26 10:08:50 +0100

    Move from swig to nunjucks, allow symantic html in description: fixes #72,  fixes #73

--- sri4node.js
+++ sri4node.js
@@ -12,7 +12,7 @@ var Validator = require('jsonschema').Validator;
 var compression = require('compression');
 var bodyParser = require('body-parser');
 var Q = require('q');
-var swig = require('swig');
+var nunjucks = require('nunjucks');
 var express = require('express');
 
 // Utility function.
@@ -1216,9 +1216,11 @@ exports = module.exports = {
 
     //to parse html pages
     app.use('/docs/static', express.static(__dirname + '/js/docs/static'));
-    app.engine('html', swig.renderFile);
     app.set('view engine', 'html');
-    app.set('views', __dirname + '/js/docs');
+    nunjucks.configure(__dirname + '/js/docs', {
+      autoescape: true,
+      express: app
+    });
 
     var emt;
 
