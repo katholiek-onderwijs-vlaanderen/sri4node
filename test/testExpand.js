e8080c56cd2611c54c61b81ab37b38ae98ce3386
Rodrigo Uroz <rodrigouroz@gmail.com>
2016-05-10 13:15:17 -0300

    New tests for #12

--- test/testExpand.js
+++ test/testExpand.js
@@ -37,17 +37,17 @@ exports = module.exports = function (base, logverbose) {
       it('should succeed with $$expanded in results array.', function () {
         return doGet(base + '/messages?expand=full', 'sabine@email.be', 'pwd')
           .then(function (response) {
-          assert.equal(response.statusCode, 200);
-          if (!response.body.results[0].$$expanded) {
-            assert.fail('Expansion was not performed !');
-          }
-          if (!response.body.results[1].$$expanded) {
-            assert.fail('Expansion was not performed !');
-          }
-          if (!response.body.results[2].$$expanded) {
-            assert.fail('Expansion was not performed !');
-          }
-        });
+            assert.equal(response.statusCode, 200);
+            if (!response.body.results[0].$$expanded) {
+              assert.fail('Expansion was not performed !');
+            }
+            if (!response.body.results[1].$$expanded) {
+              assert.fail('Expansion was not performed !');
+            }
+            if (!response.body.results[2].$$expanded) {
+              assert.fail('Expansion was not performed !');
+            }
+          });
       });
     });
 
@@ -73,12 +73,19 @@ exports = module.exports = function (base, logverbose) {
     describe('on regular resources', function () {
       it('should succeed with $$expanded as result.', function () {
         return doGet(base + '/messages/ad9ff799-7727-4193-a34a-09f3819c3479?expand=community',
-                     'sabine@email.be', 'pwd').then(function (response) {
+          'sabine@email.be', 'pwd').then(function (response) {
           assert.equal(response.statusCode, 200);
           assert.equal(response.body.$$meta.permalink, '/messages/ad9ff799-7727-4193-a34a-09f3819c3479');
           if (!response.body.community.$$expanded) {
             assert.fail('Expansion was not performed !');
           }
+        });
+      });
+
+      it('should fail due to secure function on expanded resource', function () {
+        return doGet(base + '/messages/7f5f646c-8f0b-4ce6-97ce-8549b8b78234?expand=person',
+          'sabine@email.be', 'pwd').then(function (response) {
+          assert.equal(response.statusCode, 403);
         });
       });
     });
@@ -100,13 +107,20 @@ exports = module.exports = function (base, logverbose) {
           }
         });
       });
+
+      it('should fail due to secure function on expanded resource', function () {
+        return doGet(base + '/messages?expand=results.person',
+          'sabine@email.be', 'pwd').then(function (response) {
+          assert.equal(response.statusCode, 403);
+        });
+      });
     });
 
     // Test expand=invalid send 404 Not Found.
     describe('with invalid', function () {
       it('should say \'not found\'.', function () {
         return doGet(base + '/messages/ad9ff799-7727-4193-a34a-09f3819c3479?expand=invalid',
-                     'sabine@email.be', 'pwd').then(function (response) {
+          'sabine@email.be', 'pwd').then(function (response) {
           assert.equal(response.statusCode, 404);
         });
       });
@@ -116,7 +130,7 @@ exports = module.exports = function (base, logverbose) {
     describe('on list resources', function () {
       it('should allow expanding multiple keys.', function () {
         return doGet(base + '/messages?expand=results.person,results.community',
-                     'sabine@email.be', 'pwd').then(function (response) {
+          'sabine@email.be', 'pwd').then(function (response) {
           assert.equal(response.statusCode, 200);
           debug(response.body.results[0].$$expanded);
           if (response.body.results[0].$$expanded.community.$$expanded === null) {
@@ -144,7 +158,7 @@ exports = module.exports = function (base, logverbose) {
     describe('on list resource', function () {
       it('should have executed afterread on expanded resources.', function () {
         return doGet(base + '/messages?expand=results.person,results.community',
-                     'sabine@email.be', 'pwd').then(function (response) {
+          'sabine@email.be', 'pwd').then(function (response) {
           assert.equal(response.statusCode, 200);
           if (response.body.results[0].$$expanded.community.$$expanded.$$messagecount == null) {
             assert.fail('afterread was not executed on expanded resource !');
@@ -156,7 +170,7 @@ exports = module.exports = function (base, logverbose) {
     describe('with 2 level path (x.y)', function () {
       it('should expand recursively.', function () {
         return doGet(base + '/messages?expand=results.person.community,results.community',
-                     'sabine@email.be', 'pwd').then(function (response) {
+          'sabine@email.be', 'pwd').then(function (response) {
           assert.equal(response.statusCode, 200);
           if (response.body.results[0].$$expanded.community.$$expanded === null) {
             assert.fail('Expansion was not performed !');
@@ -170,5 +184,6 @@ exports = module.exports = function (base, logverbose) {
         });
       });
     });
+
   });
 };
