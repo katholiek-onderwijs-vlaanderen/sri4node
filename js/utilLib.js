3871337e0e6a12a04d68716571542b28fad3ab1f
Johannes Govaerts <johannes.govaerts@vsko.be>
2018-02-12 17:35:04 +0100

    adapted addReferencingResources to v2.0  (https://github.com/katholiek-onderwijs-vlaanderen/sri4node/issues/140)

--- js/utilLib.js
+++ js/utilLib.js
@@ -1,3 +1,10 @@
+const pMap = require('p-map');
+
+const { typeToConfig, pgExec, transformRowToObject } = require('./common.js');
+var queryobject = require('./queryObject.js');
+const prepare = queryobject.prepareSQL; 
+const expand = require('./expand.js');
+
 exports = module.exports = {
 
   /*
@@ -6,24 +13,24 @@ exports = module.exports = {
    * column : the database column that contains the foreign key.
    * key : the name of the key to add to the retrieved elements.
    */  
-   //TODO: refactor and fix (together with the whole expand story)
-  addReferencingResources: function (type, column, targetkey, expand) {
+   //TODO: refactor in v2.1 together with the whole expand story
+  addReferencingResources: function (type, column, targetkey, toExpand) {
     'use strict';
-    return function (database, elements) {
-      var tablename, query, elementKeys, elementKeysToElement;
-      var permalink, elementKey;
-      var deferred = Q.defer();
-      var typeToMapping = typeToConfig(resources);
-      var mapping = typeToMapping[type];
+
+
+    return async function (tx, sriRequest, elements) {
+      const resources = global.sri4node_configuration.resources
+      const typeToMapping = typeToConfig(resources);
+      const mapping = typeToMapping[type];
 
       if (elements && elements.length && elements.length > 0) {
-        tablename = type.split('/')[type.split('/').length - 1];
-        query = prepare();
-        elementKeys = [];
-        elementKeysToElement = {};
-        elements.forEach(function (element) {
-          permalink = element.$$meta.permalink;
-          elementKey = permalink.split('/')[2];
+        const tablename = type.split('/')[type.split('/').length - 1];
+        const query = prepare();
+        const elementKeys = [];
+        const elementKeysToElement = {};
+        elements.forEach(function ( {stored: element} ) {
+          const permalink = element.$$meta.permalink;
+          const elementKey = permalink.split('/')[2];
           elementKeys.push(elementKey);
           elementKeysToElement[elementKey] = element;
           element[targetkey] = [];
@@ -32,43 +39,17 @@ exports = module.exports = {
         query.sql('select *, \"' + column + '\" as fkey from ' +
           tablename + ' where \"' + column + '\" in (').array(elementKeys)
           .sql(') and \"$$meta.deleted\" = false');
-        pgExec(database, query).then(function (result) {
-          result.rows.forEach(function (row) {
-            var element = elementKeysToElement[row.fkey];
-            var target = {href: type + '/' + row.key};
-            var key, referencedType;
-            if (expand) {
-              target.$$expanded = {};
-              for (key in row) {
-                if (row.hasOwnProperty(key) && row[key] && key.indexOf('$$meta.') === -1 && key !== 'fkey') {
-                  if (mapping.map[key] && mapping.map[key].references) {
-                    referencedType = mapping.map[key].references;
-                    target.$$expanded[key] = {href: typeToMapping[referencedType].type + '/' + row[key]};
-                  } else {
-                    target.$$expanded[key] = row[key];
-                  }
-                }
-              }
+        const rows = await pgExec(tx, query)
+        await pMap(rows, async (row) => {
+          const element = elementKeysToElement[row.fkey];
+          const target = {href: type + '/' + row.key};
 
-              target.$$expanded.$$meta = {
-                permalink: type + '/' + row.key,
-                schema: type + '/schema',
-                created: row['$$meta.created'],
-                modified: row['$$meta.modified']
-              };
-            }
-            element[targetkey].push(target);
-          });
-          deferred.resolve();
-        }).fail(function (e) {
-          cl(e.stack);
-          deferred.reject();
+          if (expand) {
+            target.$$expanded = await transformRowToObject(row, mapping)
+          }
+          element[targetkey].push(target);
         });
-      } else {
-        deferred.resolve();
-      }
-
-      return deferred.promise;
+      }  
     };
   }
 }
