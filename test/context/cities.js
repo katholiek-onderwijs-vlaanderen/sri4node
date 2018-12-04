0783d380373f44ebc80004f52e3f5390ab68cbff
Johannes Govaerts <johannes.govaerts@vsko.be>
2018-12-04 15:32:31 +0100

    added missing test context file

---
+++ test/context/cities.js
@@ -0,0 +1,30 @@
+var common = require('../../js/common.js');
+
+exports = module.exports = function (roa, extra) {
+  'use strict';
+
+  var $s = roa.schemaUtils;
+
+  var ret = {
+    type: '/cities',
+    'public': false, // eslint-disable-line
+    map: {
+      key: {},
+      name: {}
+    },
+    schema: {
+      $schema: 'http://json-schema.org/schema#',
+      title: 'A city.',
+      type: 'object',
+      properties: {
+        key: $s.numeric('Identifier.'),
+        name: $s.string('Name of the package.'),
+        nisCode: $s.numeric('nisCode')
+      },
+      required: ['key', 'name', 'nisCode']
+    },
+  };
+
+  common.mergeObject(extra, ret);
+  return ret;
+};
